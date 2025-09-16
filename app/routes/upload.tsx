84074a7a948bc53f034eff4fcbd26f293cddc767
William Mattsson <william.mattsson@iths.se>
2025-09-16 19:35:50 +0200

    handle form data and file on submit

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -11,7 +11,18 @@ const upload = () => {
         setFile(file)
     }
 
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {}
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const form = e.currentTarget.closest('form')
+        if (!form) return
+        const formData = new FormData(form)
+
+        const companyName = formData.get('company-name')
+        const jobTitle = formData.get('job-title')
+        const jobDescription = formData.get('job-description')
+
+        console.log({ companyName, jobTitle, jobDescription, file })
+    }
 
     return (
         <main className="bg-[url('/images/bg-main.svg')] bg-cover">
