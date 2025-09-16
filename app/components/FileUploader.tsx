6d3c0e746b0eae6d07c58a6d96504ff113901c88
William Mattsson <william.mattsson@iths.se>
2025-09-16 19:28:31 +0200

    handle file upload and show size

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,34 +1,66 @@
 import { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
+import { formatSize } from '~/lib/utils'
 
-const FileUploader = () => {
-    const [file, setFile] = useState()
+interface FileUploaderProps {
+    onFileSelect?: (file: File | null) => void
+}
 
-    const onDrop = useCallback((acceptedFiles) => {
-        // Do something with the files
-    }, [])
+const FileUploader = ({ onFileSelect }: FileUploaderProps) => {
+    const onDrop = useCallback(
+        (acceptedFiles: File[]) => {
+            const file = acceptedFiles[0] || null
 
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({
-        onDrop
-    })
+            onFileSelect?.(file)
+        },
+        [onFileSelect]
+    )
+
+    const { getRootProps, getInputProps, isDragActive, acceptedFiles } =
+        useDropzone({
+            onDrop,
+            multiple: false,
+            accept: { 'application/pdf': ['.pdf'] },
+            maxSize: 20 * 1024 * 1024
+        })
+
+    const file = acceptedFiles[0] || null
 
     return (
         <div className="w-full gradient-border">
             <div {...getRootProps()}>
                 <input {...getInputProps()} />
                 <div className="space-y-4 cursor-pointer">
-                    <div className="mx-auto w-16 h-16 flex items-center justify-center">
-                        <img
-                            src="/icons/info.svg"
-                            alt="upload"
-                            className="size-20"
-                        />
-                    </div>
-
                     {file ? (
-                        <div></div>
+                        <div
+                            className="uploader-selected-file"
+                            onClick={(e) => e.stopPropagation()}
+                        >
+                            <img
+                                src="/images/pdf.png"
+                                alt="pdf"
+                                className="size-10"
+                            />
+                            <div className="flex items-center space-x-3">
+                                <div>
+                                    <p className="text-sm text-gray-700 font-medium truncate max-w-xs">
+                                        {file.name}
+                                    </p>
+                                    <p className="text-sm text-gray-500">
+                                        {formatSize(file.size)}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
                     ) : (
                         <div>
+                            <div className="mx-auto w-16 h-16 flex items-center justify-center mb-2">
+                                <img
+                                    src="/icons/info.svg"
+                                    alt="upload"
+                                    className="size-20"
+                                />
+                            </div>
                             <p className="text-lg text-gray-500">
                                 <span className="font-semibold">
                                     Click to upload
