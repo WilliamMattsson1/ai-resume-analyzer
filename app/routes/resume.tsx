061fa1083f715f54629c9935e26d17e51a2061f5
William Mattsson <william.mattsson@iths.se>
2025-09-24 15:28:06 +0200

    add back button to return to homepage

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
-import { useParams } from 'react-router'
+import { Link, useParams } from 'react-router'
+
+export const meta = () => {
+    return [
+        { title: 'Resumind - Review' },
+        {
+            name: 'description',
+            content: 'Detailed overview of your resume'
+        }
+    ]
+}
 
 const resume = () => {
     const { id } = useParams()
 
-    return <div>Resume ID: {id}</div>
+    return (
+        <main className="!pt-0">
+            <nav className="resume-nav">
+                <Link to="/" className="back-button">
+                    <img
+                        src="/icons/back.svg"
+                        alt="logo"
+                        className="w-2.5 h-2.5"
+                    />
+                    <span className="text-gray-800 text-sm font-semibold">
+                        Back to Homepage
+                    </span>
+                </Link>
+            </nav>
+        </main>
+    )
 }
 
 export default resume
