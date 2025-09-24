0e3b905b42d9f78da9b2eab1e10295fc5a4f26cb
William Mattsson <william.mattsson@iths.se>
2025-09-24 15:47:58 +0200

    show the resume as an image

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Link, useParams } from 'react-router'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate, useParams } from 'react-router'
+import { usePuterStore } from '~/lib/puter'
 
 export const meta = () => {
     return [
@@ -12,7 +13,41 @@ export const meta = () => {
 }
 
 const resume = () => {
+    const { auth, isLoading, fs, kv } = usePuterStore()
     const { id } = useParams()
+
+    const [imageUrl, setImageUrl] = useState('')
+    const [resumeUrl, setResumeUrl] = useState('')
+    const [feedback, setFeedback] = useState('')
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const loadResume = async () => {
+            const resume = await kv.get(`resume:${id}`)
+
+            if (!resume) return
+
+            const data = JSON.parse(resume)
+
+            const resumeBlob = await fs.read(data.resumePath)
+            if (!resumeBlob) return
+
+            const pdfBlob = new Blob([resumeBlob], { type: 'application/pdf' })
+            const resumeUrl = URL.createObjectURL(pdfBlob)
+            setResumeUrl(resumeUrl)
+
+            const imageBlob = await fs.read(data.imagePath)
+            if (!imageBlob) return
+
+            const imageURL = URL.createObjectURL(imageBlob)
+            setImageUrl(imageURL)
+
+            setFeedback(data.feedback)
+            console.log({ resumeUrl, imageURL, feedback: data.feedback })
+        }
+
+        loadResume()
+    }, [id])
 
     return (
         <main className="!pt-0">
@@ -28,6 +63,22 @@ const resume = () => {
                     </span>
                 </Link>
             </nav>
+            <div className="flex flex-row w-full max-lg:flex-col-reverse">
+                <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-around]">
+                    {imageUrl && resumeUrl && (
+                        <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
+                            <a>
+                                <img
+                                    src={imageUrl}
+                                    alt="resume-image"
+                                    className="w-full h-full object-contain rounded-2xl "
+                                    title="Resume"
+                                />
+                            </a>
+                        </div>
+                    )}
+                </section>
+            </div>
         </main>
     )
 }
