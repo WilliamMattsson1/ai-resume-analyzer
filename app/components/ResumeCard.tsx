603505f699a592ef1f4fca6c09a33669e91f1a1e
William Mattsson <william.mattsson@iths.se>
2025-10-01 16:03:40 +0200

    handle if no company details is entered by the user

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router'
 import ScoreCircle from './ScoreCircle'
+import { usePuterStore } from '~/lib/puter'
 
 const ResumeCard = ({
     resume: { id, companyName, jobTitle, feedback, resumePath, imagePath }
 }: {
     resume: Resume
 }) => {
+    const { fs } = usePuterStore()
+    const [resumeUrl, setResumeUrl] = useState('')
+
+    useEffect(() => {
+        const loadResume = async () => {
+            const blob = await fs.read(imagePath)
+            if (!blob) return
+            let url = URL.createObjectURL(blob)
+            setResumeUrl(url)
+        }
+    }, [imagePath])
+
     return (
         <Link
             to={`/resume/${id}`}
@@ -14,12 +27,19 @@ const ResumeCard = ({
         >
             <div className="resume-card-header">
                 <div className="flex flex-col gap-2">
-                    <h2 className="!text-black font-bold break-words">
-                        {companyName}
-                    </h2>
-                    <h3 className="text-lg break-words text-gray-500">
-                        {jobTitle}
-                    </h3>
+                    {companyName && (
+                        <h2 className="!text-black font-bold break-words">
+                            {companyName}
+                        </h2>
+                    )}
+                    {jobTitle && (
+                        <h3 className="text-lg break-words text-gray-500">
+                            {jobTitle}
+                        </h3>
+                    )}
+                    {!companyName && !jobTitle && (
+                        <h2 className="!text-black font-bold">Resume</h2>
+                    )}
                 </div>
                 <div className="flex-shrink-0">
                     <ScoreCircle score={feedback.overallScore} />
