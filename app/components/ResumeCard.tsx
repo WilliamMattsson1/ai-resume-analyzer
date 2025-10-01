662d3b28e93375049be3592c743b774bbacb55a8
William Mattsson <william.mattsson@iths.se>
2025-10-01 16:23:05 +0200

    fetch uploaded resumes and handle  all cases

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -45,15 +45,17 @@ const ResumeCard = ({
                     <ScoreCircle score={feedback.overallScore} />
                 </div>
             </div>
-            <div className="gradient-border animate-in fade-in duration-1000">
-                <div className="w-full h-full">
-                    <img
-                        src={imagePath}
-                        alt="resume"
-                        className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
-                    />
+            {resumeUrl && (
+                <div className="gradient-border animate-in fade-in duration-1000">
+                    <div className="w-full h-full">
+                        <img
+                            src={imagePath}
+                            alt="resume"
+                            className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+                        />
+                    </div>
                 </div>
-            </div>
+            )}
         </Link>
     )
 }
